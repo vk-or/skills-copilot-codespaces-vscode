41d293b12a649a48195cd1314ed400fa45556e64
Viacheslav Kudinov <viacheslav@kudinov.tech>
2023-12-24 20:27:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/components/member/member.html',
+        controller: 'MemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: '=',
+            skills: '='
+        }
+    };
+}
